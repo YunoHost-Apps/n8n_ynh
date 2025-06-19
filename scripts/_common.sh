9246a4cb6c605f0f49d53ca128d6bb1f2f5c0ff0
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-06-19 10:29:20 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-nodejs_version=22
+nodejs_version="22"
 
 timezone="$(timedatectl show --value --property=Timezone)"
 main_domain=$(cat /etc/yunohost/current_host)
